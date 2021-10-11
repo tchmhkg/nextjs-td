39e7104bb179aa95c9ea518974e61d54fcd4ac3d
tchmhkg <26540312+tchmhkg@users.noreply.github.com>
2021-10-12 01:54:09 +0800

    update auth body

--- pages/api/auth.js
+++ pages/api/auth.js
@@ -2,18 +2,17 @@ import axios from "axios";
 
 export default async function handler(req, res) {
   const redirect_uri = process.env.REDIRECT_HOST+'/auth';
-  console.log('process.env',process.env)
   
   const base_url = 'https://api.tdameritrade.com/v1';
 
   try {
-    const response = await axios.post(base_url + "/oauth2/token", {form: {
+    const response = await axios.post(base_url + "/oauth2/token", {
       grant_type: "authorization_code",
       access_type: "offline",
       code: req.query.code, // get the code from url
       client_id: process.env.CLIENT_ID + "@AMER.OAUTHAP", // client id stored in heroku
       redirect_uri: redirect_uri,
-    }}, {headers: {
+    }, {headers: {
       "Content-Type": "application/x-www-form-urlencoded",
     }});
 
