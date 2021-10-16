005488c21eda88c9d164d82c40b070aea1d4dead
tchmhkg <26540312+tchmhkg@users.noreply.github.com>
2021-10-17 02:27:36 +0800

    fix api error

--- next.config.js
+++ next.config.js
@@ -3,4 +3,7 @@ module.exports = {
     REDIRECT_HOST: process.env.REDIRECT_HOST,
     CLIENT_ID: process.env.CLIENT_ID,
   },
+  api: {
+    externalResolver: true,
+  },
 };
--- pages/api/auth.js
+++ pages/api/auth.js
@@ -1,17 +1,27 @@
 import axios from "axios";
+import url from 'url';
 
 export default async function handler(req, res) {
   const redirect_uri = process.env.REDIRECT_HOST+'/auth';
   
   const base_url = 'https://api.tdameritrade.com/v1';
 
-  const query = `?grant_type=authorization_code&refresh_token=&access_type=offline&code=${encodeURIComponent(req.query.code || '')}&client_id=${encodeURIComponent(process.env.CLIENT_ID + "@AMER.OAUTHAP")}&redirect_uri=${encodeURIComponent(redirect_uri)}`
-  console.log('Query:',query);
+  const apiUrl = base_url + "/oauth2/token"
+
+  const data = {
+    grant_type: "authorization_code",
+    access_type: "offline",
+    code: decodeURIComponent(req.query.code || ''),
+    client_id: process.env.CLIENT_ID + "@AMER.OAUTHAP",
+    redirect_uri: redirect_uri
+  }
+  const params = new url.URLSearchParams(data);
+
+  console.log('Params:',params);
+  console.log("Url:",apiUrl);
 
   try {
-    const response = await axios.post(base_url + "/oauth2/token" + query, {}, {headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    }});
+    const response = await axios.post(apiUrl, params);
 
     console.log("API Response:", response.data);
     res.send(response.data);
